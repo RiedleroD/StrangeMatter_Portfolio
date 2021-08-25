92745e1417a0f3c62287aaa47b11ab293bcd4086
Riedler <riedler@gelse.net>
2021-08-25 14:55:10 +0200

    changed from local to github config & data; fixes #2

--- text_helper.php
+++ text_helper.php
@@ -3,12 +3,11 @@ function escape($text){
 	return str_replace("\n","<br/>",htmlentities($text));
 }
 function get_data(){
-	return json_decode(file_get_contents("./data.json"),true,512,JSON_THROW_ON_ERROR);
+	return json_decode(file_get_contents("https://raw.githubusercontent.com/RiedleroD/StrangeMatter_Portfolio/data/data.json"),true,512,JSON_THROW_ON_ERROR);
 }
 function parse_source($src){
 	if($src[0] == '$'){
-		$src[0]='/';
-		return "./imgs$src";
+		return "https://raw.githubusercontent.com/RiedleroD/StrangeMatter_Portfolio/data/imgs/".urlencode(ltrim($src,'$'));
 	}else{
 		return $src;
 	}
